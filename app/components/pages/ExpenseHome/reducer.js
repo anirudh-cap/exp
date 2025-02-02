58ae47d85986cb4fb596a8c870aff27aa54fb335
Anirudh Kini <anirudhkini@CLPT2155.local>
2025-02-02 14:56:41 +0530

    fix

--- app/components/pages/ExpenseHome/reducer.js
+++ app/components/pages/ExpenseHome/reducer.js
@@ -1,5 +1,5 @@
-import * as types from './constants';
 import { fromJS } from 'immutable';
+import * as types from './constants';
 
 export const initialState = fromJS({
   expenses: [],
--- app/components/pages/ExpenseHome/saga.js
+++ app/components/pages/ExpenseHome/saga.js
@@ -1,82 +1,82 @@
 import { call, put, all, takeLatest } from 'redux-saga/effects';
 import * as types from './constants';
 
-const JsonUrl = "http://localhost:8001/expenses";
+const JsonUrl = 'http://localhost:8001/expenses';
 
 function* fetchExpenseSaga() {
-    try {
-        const response = yield call(fetch, JsonUrl);
-        const data = yield response.json();
-        yield put({ type: types.FETCH_EXPENSE_SUCCESS, payload: data });
-    } catch (error) {
-        yield put({ type: types.FETCH_EXPENSE_FAILURE, payload: error.message });
-    }
+  try {
+    const response = yield call(fetch, JsonUrl);
+    const data = yield response.json();
+    yield put({ type: types.FETCH_EXPENSE_SUCCESS, payload: data });
+  } catch (error) {
+    yield put({ type: types.FETCH_EXPENSE_FAILURE, payload: error.message });
+  }
 }
 
 function* addExpenseRequest(action) {
-    try {
-        const response = yield call(() =>
-            fetch(JsonUrl, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(action.payload),
-            }).then(res => res.json())
-        );
-        yield put({ type: types.ADD_EXPENSE_SUCCESS, payload: response });
-    } catch (error) {
-        yield put({ type: types.ADD_EXPENSE_FAILURE, payload: error });
-    }
+  try {
+    const response = yield call(() =>
+      fetch(JsonUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(action.payload),
+      }).then(res => res.json()),
+    );
+    yield put({ type: types.ADD_EXPENSE_SUCCESS, payload: response });
+  } catch (error) {
+    yield put({ type: types.ADD_EXPENSE_FAILURE, payload: error });
+  }
 }
 
 function* editExpenseRequest(action) {
-    try {
-        const response = yield call(() =>
-            fetch(`${JsonUrl}/${action.payload.id}`, {
-                method: "PUT",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(action.payload.updatedData),
-            }).then(res => res.json())
-        );
-        yield put({ type: types.EDIT_EXPENSE_SUCCESS, payload: response });
-    } catch (error) {
-        yield put({ type: types.EDIT_EXPENSE_FAILURE, payload: error });
-    }
+  try {
+    const response = yield call(() =>
+      fetch(`${JsonUrl}/${action.payload.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(action.payload.updatedData),
+      }).then(res => res.json()),
+    );
+    yield put({ type: types.EDIT_EXPENSE_SUCCESS, payload: response });
+  } catch (error) {
+    yield put({ type: types.EDIT_EXPENSE_FAILURE, payload: error });
+  }
 }
 
 function* delExpenseRequest(action) {
-    try {
-        yield call(() =>
-            fetch(`${JsonUrl}/${action.payload}`, {
-                method: "DELETE",
-            })
-        );
-        yield put({ type: types.DELETE_EXPENSE_SUCCESS, payload: action.payload });
-    } catch (error) {
-        yield put({ type: types.DELETE_EXPENSE_FAILURE, payload: error });
-    }
+  try {
+    yield call(() =>
+      fetch(`${JsonUrl}/${action.payload}`, {
+        method: 'DELETE',
+      }),
+    );
+    yield put({ type: types.DELETE_EXPENSE_SUCCESS, payload: action.payload });
+  } catch (error) {
+    yield put({ type: types.DELETE_EXPENSE_FAILURE, payload: error });
+  }
 }
 
 export function* watchAddExpenseRequests() {
-    yield takeLatest(types.ADD_EXPENSE_REQUEST, addExpenseRequest);
+  yield takeLatest(types.ADD_EXPENSE_REQUEST, addExpenseRequest);
 }
 
 export function* watchDeleteExpenseRequests() {
-    yield takeLatest(types.DELETE_EXPENSE_REQUEST, delExpenseRequest);
+  yield takeLatest(types.DELETE_EXPENSE_REQUEST, delExpenseRequest);
 }
 
 export function* watchFetchExpenseRequests() {
-    yield takeLatest(types.FETCH_EXPENSE_REQUEST, fetchExpenseSaga);
+  yield takeLatest(types.FETCH_EXPENSE_REQUEST, fetchExpenseSaga);
 }
 
 export function* watchEditExpenseRequests() {
-    yield takeLatest(types.EDIT_EXPENSE_REQUEST, editExpenseRequest);
+  yield takeLatest(types.EDIT_EXPENSE_REQUEST, editExpenseRequest);
 }
 
 export default function*() {
-    yield all([
-        watchAddExpenseRequests(),
-        watchDeleteExpenseRequests(),
-        watchFetchExpenseRequests(),
-        watchEditExpenseRequests(),
-    ]);
+  yield all([
+    watchAddExpenseRequests(),
+    watchDeleteExpenseRequests(),
+    watchFetchExpenseRequests(),
+    watchEditExpenseRequests(),
+  ]);
 }
